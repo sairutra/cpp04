1209b1b92a3c700f55115b7af7d1f8ed6bbeb336
sebastianpenning <whimizera@mailbox.org>
2025-02-11 22:48:43 +0100

    added print statements in main.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -9,13 +9,18 @@ int main ()
 	const Animal* i = new Cat();
 	const WrongAnimal* k = new WrongCat();
 
+	std::cout << std::endl;
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	std::cout << k->getType() << " " << std::endl;
+
+	std::cout << std::endl;
 	j->makeSound();
 	i->makeSound();
 	meta->makeSound();
 	k->makeSound();
+	
+	std::cout << std::endl;
 	delete meta;
 	delete i;
 	delete j;
